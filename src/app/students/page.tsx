4219723f8a389e503418268581a402799164efe7
Mizanur Rahman <mr.milonku@hotmail.com>
2024-12-19 11:30:28 +0800

    added some listing

--- src/app/students/page.tsx
+++ src/app/students/page.tsx
@@ -1,7 +1,120 @@
+"use client";
+
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "studentId", headerName: "ID", width: 120 },
+  { field: "registrationId", headerName: "Registration ID", width: 120 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "dateofbirth", headerName: "Date of Birth", width: 150 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "contact", headerName: "Contact", width: 200 },
+  { field: "identificationnumber", headerName: "NRIC/Passport", width: 200 },
+  { field: "nationality", headerName: "Nationality", width: 200 },
+  { field: "enrollmentyear", headerName: "Enrollment On", width: 150 },
+  { field: "isactive", headerName: "Status", width: 150 },
+];
+
 const Students = () => {
-    return (
-      <div>Students</div>
-    )
-  }
+  const students = [
+    {
+      "studentId": "2024021001",
+      "registrationId": "S202412R001",
+      "name": "Student 101",
+      "dateofbirth": "01 Jan, 2012",
+      "email": "student101@madrasah.com.my",
+      "contact": "+60-13 365 6524",
+      "identificationnumber": "154-254-3541",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021002",
+      "registrationId": "S202412R002",
+      "name": "Student 102",
+      "dateofbirth": "01 Feb, 2012",
+      "email": "student102@madrasah.com.my",
+      "contact": "+60-12 365 6522",
+      "identificationnumber": "124-254-3531",
+      "nationality": "Bangladesh",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021003",
+      "registrationId": "S202412R003",
+      "name": "Student 103",
+      "dateofbirth": "05 Jan, 2012",
+      "email": "student103@madrasah.com.my",
+      "contact": "+60-13 325 6521",
+      "identificationnumber": "174-251-3581",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021004",
+      "registrationId": "S202412R004",
+      "name": "Student 104",
+      "dateofbirth": "12 Jan, 2012",
+      "email": "student104@madrasah.com.my",
+      "contact": "+60-14 368 6522",
+      "identificationnumber": "134-214-3843",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021005",
+      "registrationId": "S202412R005",
+      "name": "Student 105",
+      "dateofbirth": "09 Jan, 2012",
+      "email": "student105@madrasah.com.my",
+      "contact": "+60-11 362 6527",
+      "identificationnumber": "134-274-3543",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021006",
+      "registrationId": "S202412R006",
+      "name": "Student 106",
+      "dateofbirth": "07 Jan, 2012",
+      "email": "student106@madrasah.com.my",
+      "contact": "+60-13 387 6374",
+      "identificationnumber": "161-254-5314",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+    {
+      "studentId": "2024021007",
+      "registrationId": "S202412R007",
+      "name": "Student 107",
+      "dateofbirth": "10 Jan, 2012",
+      "email": "student107@madrasah.com.my",
+      "contact": "+60-11 369 6533",
+      "identificationnumber": "155-256-3511",
+      "nationality": "Malaysian",
+      "enrollmentyear": "2024",
+      "isactive": "Active",
+    },
+  ]
+  return (
+    <div className="flex flex-col">
+      <Header name="Students" />
+      <DataGrid
+        rows={students}
+        columns={columns}
+        getRowId={(row) => row.studentId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+      />
+    </div>
+  );
+}
   
   export default Students
--- src/app/teachers/page.tsx
+++ src/app/teachers/page.tsx
@@ -1,7 +1,73 @@
+"use client";
+
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+const columns: GridColDef[] = [
+  { field: "teacherId", headerName: "ID", width: 120 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "email", headerName: "Email", width: 200 },
+  { field: "contact", headerName: "Contact", width: 200 },
+  { field: "identificationnumber", headerName: "NRIC/Passport", width: 200 },
+  { field: "nationality", headerName: "Nationality", width: 200 },
+  { field: "position", headerName: "Position", width: 150 },
+  { field: "dateOfJoining", headerName: "Date of Joining", width: 150 },
+  { field: "specialized", headerName: "Specialized", width: 150 },
+  { field: "isactive", headerName: "Status", width: 150 },
+];
+
 const Teachers = () => {
+  const teachers = [
+    {
+      "teacherId": "T2024021001",
+      "name": "Teacher 101",
+      "email": "teacher101@madrasah.com.my",
+      "contact": "+60-13 365 6524",
+      "identificationnumber": "154-254-3541",
+      "nationality": "Malaysian",
+      "position": "Lecturer",
+      "dateOfJoining": "01 Jan 2015",
+      "specialized": "Bukhari Part 1",
+      "isactive": "Active",
+    },
+    {
+      "teacherId": "T2024021002",
+      "name": "Teacher 102",
+      "email": "teacher102@madrasah.com.my",
+      "contact": "+60-11 365 6512",
+      "identificationnumber": "144-254-3512",
+      "nationality": "Malaysian",
+      "position": "Lecturer",
+      "dateOfJoining": "01 Jan 2015",
+      "specialized": "Bukhari Part 2",
+      "isactive": "Active",
+    },
+    {
+      "teacherId": "T2024021003",
+      "name": "Teacher 103",
+      "email": "teacher103@madrasah.com.my",
+      "contact": "+60-13 364 6587",
+      "identificationnumber": "144-254-3541",
+      "nationality": "Malaysian",
+      "position": "Assistant Lecturer",
+      "dateOfJoining": "01 Jan 2015",
+      "specialized": "Bukhari Part 3",
+      "isactive": "Active",
+    },
+  ];
+
     return (
-      <div>Teachers</div>
-    )
-  }
+      <div className="flex flex-col">
+        <Header name="Users" />
+        <DataGrid
+          rows={teachers}
+          columns={columns}
+          getRowId={(row) => row.teacherId}
+          checkboxSelection
+          className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+        />
+      </div>
+    );
+}
   
   export default Teachers
